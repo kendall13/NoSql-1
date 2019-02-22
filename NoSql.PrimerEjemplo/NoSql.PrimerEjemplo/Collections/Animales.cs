25cc86d47963d74f3fcafc692b8f3ac8024364b2
Laboratorios <Laboratorios@Q19>
2019-02-21 20:49:34 -0600

    alo

---
+++ NoSql.PrimerEjemplo/NoSql.PrimerEjemplo/Collections/Animales.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoSql.PrimerEjemplo.Collections
+{
+
+    /*
+{
+    "_id" : 1,
+    "nombre" : "saulito",
+    "fechaNacimiento" : ISODate("2019-01-01T21:21:20.201Z"),
+    "tipo" : "perro",
+    "tratamiento" : [
+        {
+            "tipo" : "operacion",
+            "fecha" : ISODate("2019-02-01T22:21:20.201Z"),
+            "precio" : 500
+        }
+    ]
+}
+
+         */
+
+    public class Animales
+    {
+        ObjectId _id;
+        string nombre;
+        DateTime fechaNacimiento;
+        string tipo;
+        IList <Tratamiento> tratamiento;      
+
+    }
+
+    public class Tratamiento
+    {
+
+        string tipo;
+        DateTime fecha;
+        decimal precio;
+    }
+}
---
+++ NoSql.PrimerEjemplo/NoSql.PrimerEjemplo/Controllers/AnimalController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using NoSql.PrimerEjemplo.Collections;
+
+namespace NoSql.PrimerEjemplo.Controllers
+{
+    public class AnimalController : Controller
+    {
+        static public MongoClient server = new MongoClient(ConfigurationManager.AppSettings.Get("mongoConnect"));
+        static public IMongoDatabase db = server.GetDatabase(ConfigurationManager.AppSettings.Get("databaseName"));
+        public AnimalController()
+        {
+
+            
+            var animaes = db.GetCollection<Animales>("animales");
+        }
+        // GET: Animal
+        public ActionResult Index()
+        {
+            
+            return Json(db.Settings);
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
